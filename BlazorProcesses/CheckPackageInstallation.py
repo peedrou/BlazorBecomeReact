dc4848138c148c0892b6ea9400b8c33034fb228f
peedrou <pedrofsalesvieira@gmail.com>
2023-03-13 15:02:31 +0000

    Added CheckPackage

---
+++ BlazorProcesses/CheckPackageInstallation.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+@dataclass
+class Package:
+
+    PackageInstalled: bool = False
+
+    def CheckPackage(self):
+        answer = input("Have you installed the package (microsoft.aspnetcore.components.customelements) in your Blazor Project? [Y/N] ")
+        if answer == "Y" or "y":
+            self.PackageInstalled = True
+            return self.PackageInstalled
+        elif answer == "N" or "n":
+            print("Please install the package before continuing")
+            return
+        else:
+            print("Invalid input, please enter Y or N.")
+            return
+    
+    def test(self):
+        print(self.PackageInstalled)
+
--- Main.py
+++ Main.py
@@ -1,11 +1,19 @@
 from dataclasses import dataclass
+from .BlazorProcesses.CheckPackageInstallation import Package
 import typer
 
 @dataclass
 class BBR:
-    ReactRootComponent: str
-    BlazorRootComponent: str
+    ReactRootComponentFolder: str
+    BlazorRootComponentFolder: str
+    BlazorProgramFolder: str
 
-    def ConvertAllRegisteredComponents(ReactRootComponent, BlazorRootComponent):
+    def ConvertAllRegisteredComponents(ReactRootComponent, BlazorRootComponent) -> None:
+        pass
+
+    def ConvertComponent(ReactRootComponent, BlazorRootComponent, ComponentName: str)-> None:
+        pass
+
+    def CreateCustomComponent(BlazorProgramFolder, RazorWidgetName: str, NewComponentName: str) -> None:
         pass
     
