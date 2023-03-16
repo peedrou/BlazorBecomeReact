9f43b890ea4e474baf5740acebb0bf409fe72808
peedrou <pedrofsalesvieira@gmail.com>
2023-03-16 15:05:14 +0000

    Added test and Write to Program Function

--- BlazorProcesses/NonClassFunctions.py
+++ BlazorProcesses/NonClassFunctions.py
@@ -1,3 +1,29 @@
-def WriteToProgramFile(code, path) -> None:
-    with open(path, 'a') as file:
-       file.write(code + '\n')
+def WriteToProgramFile(code, path) -> bool:
+    variable_name = "var builder"
+    variable_line = None
+
+    try:
+        with open(path, 'r') as file:
+            contents = file.read()
+            lines = contents.splitlines()
+
+        for i, line in enumerate(lines):
+            if variable_name in line:
+                variable_line = i
+                break
+
+        if variable_line is None:
+            raise ValueError(f"Variable {variable_name} not found in file.")
+
+        with open(path, 'r+') as file:
+            file.seek(0)
+            file.write('\n'.join(lines[:variable_line+1]) + '\n' + code + '\n' + '\n'.join(lines[variable_line+1:]))
+            file.truncate()
+        
+        print("here")
+        return True
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
